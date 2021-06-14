30a50bbe5ccd255a410aaf92c276323023ff8e22
ed.lutsyk <ed.lutsyk@ekreative.com>
2021-06-15 00:20:35 +0300

    Finished category

--- src/components/pages/category/index.jsx
+++ src/components/pages/category/index.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 import './styles.scss';
 import MonthSlider from "../../core/month-slider";
@@ -8,27 +8,29 @@ import {Doughnut} from "react-chartjs-2";
 import CategoryList from "../../core/category-list";
 
 
-const dataChart = {
-    datasets: [
-        {
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                '#032c55',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-        },
-    ],
-
+const createDataset = (data, colors) => {
+    return {
+        datasets: [{
+            data: data,
+            backgroundColor: colors,
+        }]
+    }
 };
+
+
+const defaultDataset = () => {
+    return createDataset([1], ['#cccccc']);
+}
+
+const itemsDataset = (items) => {
+    return createDataset(items.map(item => item.totalByCategory), items.map(item => item.color));
+}
 
 const chartOptions = {
     cutout: 105,
     responsive: true,
     maintainAspectRatio: true,
+    animation: false,
     plugins: {
         tooltip: {
             enabled: false
@@ -39,12 +41,11 @@ const chartOptions = {
 const Category = () => {
     const userData = useSelector(state => state.user);
     const [items, setItems] = useState([]);
-    // const [dataSet, setDataSet] = useState([]);
     const [selectedDate, setSelectedDate] = useState(null);
 
     useEffect(() => {
-        console.log('selected date', selectedDate)
         if (selectedDate) {
+            chartOptions.animation = true;
             handleCategories(selectedDate.get('month'), selectedDate.get('year')).catch(error => {
                 console.log('handled categories by selected date error', error)
             });
@@ -58,30 +59,28 @@ const Category = () => {
     }).then((data) => {
         console.log('data', data);
         setItems(data.data);
-        // setDataSet()
     });
+
+    const calculateTotal = (items) => {
+        return items.reduce((accumulator, currentValue) => accumulator + currentValue.totalByCategory, 0);
+    }
 
     return (
         <div className="category-wrapper">
             <MonthSlider onChange={setSelectedDate}/>
-            <div className="chart">
-                <div className="spending">
-                    <div className="spending-content">
-                        <p>Витрати</p>
-                        <p className="total">{items.reduce((accumulator, currentValue) => accumulator + currentValue.totalByCategory, 0)} ₴</p>
+            {items && items.length > 0 ?
+                <div className="chart">
+                    <div className="spending">
+                        <div className="spending-content">
+                            <p>Витрати</p>
+                            <p className="total">{calculateTotal(items)} ₴</p>
+                        </div>
                     </div>
-                </div>
-                {/*{console.log('dataset render', dataSet, dataSet.datasets)}*/}
-                {items && items.length >0 ? <Doughnut data={{
-                    datasets: [
-                        {
-                            data: items.map(item => item.totalByCategory),
-                            backgroundColor: items.map(item => item.color),
-                        },
-                    ]
-                }} options={chartOptions}/>: null}
-
-            </div>
+                    {calculateTotal(items) !== 0 ?
+                        <Doughnut data={itemsDataset(items)} options={chartOptions}/> :
+                        <Doughnut data={defaultDataset} options={chartOptions}/>
+                    }
+                </div> : null}
             <CategoryList data={items}/>
         </div>
     );
